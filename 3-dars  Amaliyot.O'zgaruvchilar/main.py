082b41df3c096007d00b40418e5e8c09bfe48e20
nurbekdev <88614410+nurbekdev@users.noreply.github.com>
2021-10-11 17:14:15 +0500

    main.py fayl yaratildi
    Vazifalar bajarildi

---
+++ 3-dars  Amaliyot.O'zgaruvchilar/main.py
@@ -0,0 +1,34 @@
+"""
+Created on Mon Oct 11 17:04:34 2021
+
+@author: Nurbek po'latov
+
+Mavzu: 3-bob O'zgaruvchilar Amaliyot
+
+O'qituvchi: Anvar Narzullayev 
+
+"""
+# 1-vazifa
+
+matn = "Hello world!" # matn o'zgaruvchini "hello world!" ga tenglashtirdim
+print(matn) #matnni konsolga chiqar
+
+#2-vazifa
+
+xabar = "Nurbek Po'latov"
+print(xabar)
+xabar = "python"
+print(xabar)
+
+#3-vazifa
+
+#class deb nomlangan o'zgaruvchi yarating deyilgan ekan, class bu maxsus kalit so'z
+
+#4-vazifa
+
+radius = 5
+pi = 3.14159
+yuza = pi * radius**2
+print("Radiusi " , radius, " ga teng doira yuzi = ",yuza)
+
+
