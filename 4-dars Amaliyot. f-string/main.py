f1bb2a446ad9b17ed8ae0c2a4ee8fe64398dd4b8
nurbekdev <88614410+nurbekdev@users.noreply.github.com>
2021-10-11 18:13:19 +0500

    main.py

---
+++ 4-dars Amaliyot. f-string/main.py
@@ -0,0 +1,36 @@
+"""
+Created on Mon Oct 11 18:03:20 2021
+
+@author: Nurbek Po'latov'
+"""
+#1-vazifa
+
+kocha = "Ishonpir"
+mahalla = "Qoziguzar"
+tuman = "Shahrisabz"
+viloyat = "Qashqadaryo"
+
+#2-vazifa
+
+print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+
+#3-vazifa
+
+print("Assalomu alaykum")
+kocha = input("Ko'changizni kiriting: ")
+mahalla = input("Mahallangizni kiriting: ")
+tuman = input("Tumaningzini kiriting: ")
+viloyat = input("Viloyatingizni kiriting: ")
+print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+
+#4-vazifa
+
+print(f"{kocha} ko'chasi,\n {mahalla} mahallasi,\n {tuman} tumani,\n {viloyat} viloyati")
+
+
+
+
+
+
+
+
