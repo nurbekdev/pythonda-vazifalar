c1762b0066842ada507716bb02422e4c85b333e6
nurbekdev <88614410+nurbekdev@users.noreply.github.com>
2021-10-11 16:58:51 +0500

    Create main.py
    2-darsim python Arifmetik amallar

---
+++ 2-dars Amaliyot. Arifmetik amallar/main.py
@@ -0,0 +1,25 @@
+"""
+Created on Mon Oct 11 16:49:41 2021
+
+@author: Nurbek Po'latov
+"""
+# 1-vazifa
+
+print("\"Nexia\", \"Tico\", 'Damas' ko'rganlar qilar havas")
+
+# 2-vazifa
+
+print('5 ning 4-darajasi', 5**4)  #  ' ** ' Eksponenta (darja/ildiz)
+
+print("22 ni 4 ga bo'lganda qoldiq ", 22%4)  #qoldiqli bo'lish
+
+print("Tomonlari 125 ga teng kvadratning yuzi", 125*125, "ga, perimetri", 4*125, "teng")
+
+print('Diametri 12 ga teng bo\'lgan doiraning yuzi', 3.14*(12/2)**2, 'ga teng')
+
+# 3-vazifa
+
+print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi", (6**2+7**2)**(1/2))
+
+
+
