b703df11065f9fe4dbdc87d4b55adcabd01cdd20
nurbekdev <88614410+nurbekdev@users.noreply.github.com>
2021-10-11 16:48:36 +0500

    Yangi fayl yaratildi hello.py
    Pytohnda 1-vazifam

---
+++ 1-dars Amaliyot/hello.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 11 16:43:07 2021
+
+@author: Nurbek Po'latov
+
+Pytohnda eng birinchi qilgan vazifam print("Hello world")
+"""
+# Hello world so'zini konsolga chiqarish
+print("Hello world") 
+print("Nurbek Po'latov")
+print("Assalomu alaykum")
+
