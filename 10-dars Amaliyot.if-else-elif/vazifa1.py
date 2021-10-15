3515edb6bd1d7713e2e173b2c664456d8a2c3974
nurbekdev <88614410+nurbekdev@users.noreply.github.com>
2021-10-15 15:38:26 +0500

    vazifa1.py

---
+++ 10-dars Amaliyot.if-else-elif/vazifa1.py
@@ -0,0 +1,14 @@
+"""
+Created on Fri Oct 15 15:34:09 2021
+
+@author: Nurbek Po'latov
+
+"""
+#1-vazifa
+
+son = int(input("Iltimos juft son kiriting: "))
+if son%2==0:
+    print("Rahmat!")
+else:
+    print("Bu son juft emas")
+
